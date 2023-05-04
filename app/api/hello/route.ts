e9787807f4bd9b6575af70ccdfa08c79fe8b9323
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-05-04 20:34:35 +0600

    Start Up Project

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,38 @@
+import type { NextApiResponse } from 'next';
+import prisma from "@/prisma/client";
+
+interface Data {
+	name: String;
+}
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+	// const user = await prisma.user.create({
+	// 	data: {
+	// 		name: "Alice",
+	// 		email: "alice@prisma.io",
+	// 		posts: {
+	// 			create: { title: "Hello World" },
+	// 		},
+	// 		profile: {
+	// 			create: { bio: "I like turtles" },
+	// 		},
+	// 	},
+	// });
+	// const user = await prisma.user.findFirst();
+
+	// const posts = await prisma.post.create({
+	// 	data: {
+	// 		title: "Hello World",
+	// 		authorId: user.id,
+	// 	},
+	// });
+
+	const posts = await prisma.post.findMany({
+		include: {
+			author: true,
+		},
+	});
+
+	return Response<NextApiResponse>.json({ posts });
+	// return Response.json({ name: "John Doe", request, allUsers, user });
 }
